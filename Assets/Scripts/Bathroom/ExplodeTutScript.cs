678662e4deb4992df4ff7952ceb2c09f114d8618
natalielh <nlehuenen@gmail.com>
2019-03-30 21:28:02 -0400

    Shatter edits
    Also reorganized things by moving shatter prefabs and models around to their appropriate "Shatter" folders

--- Assets/Scripts/Bathroom/ExplodeTutScript.cs
+++ Assets/Scripts/Bathroom/ExplodeTutScript.cs
@@ -4,10 +4,15 @@ using UnityEngine;
 
 public class ExplodeTutScript : MonoBehaviour
 {
-    public float destroyDelay;
-    public float minForce;
-    public float maxForce;
-    public float radius;
+    // some default values are included here, feel free to change them!!
+    [Range(0.1f, 60.0f)]
+    public float destroyDelay = 5.0f;
+    [Range(0.0f, 500.0f)]
+    public float minForce = 50.0f;
+    [Range(50.0f, 1000.0f)]
+    public float maxForce = 400.0f;
+    [Range(0.1f, 10.0f)]
+    public float radius = 0.4f;
 
 
     void Start()
@@ -15,10 +20,10 @@ public class ExplodeTutScript : MonoBehaviour
         Explode();
     }
 
-    private void OnMouseDown()
-    {
-        //Explode();
-    }
+    //private void OnMouseDown()
+    //{
+    //    //Explode();
+    //}
 
     public void Explode()
     {
---
+++ Assets/Scripts/Misc/IgnoreCollisionsWithOther.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IgnoreCollisionsWithOther : MonoBehaviour
+{
+    //attach this script to an object that you don't want to collide with a list of OTHER objects
+    //collider(s) on itself AND on children objects will be ignorned!!!
+
+    public List<GameObject> objectsToIgnore = new List<GameObject>();
+
+    //[HideInInspector]
+    public List<Collider> otherCols = new List<Collider>();
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        SetupCollisionsIgnore();
+    }
+
+    public void SetupCollisionsIgnore()
+    {
+        // please don't mind the mess... not an official programmer here..... it just works
+
+        //the colliders in THIS OBJECT
+        Component[] thisCols = this.gameObject.GetComponentsInChildren(typeof(Collider));
+
+        //add the colliders of OTHER OBJECTS to the otherCols big list
+        foreach (GameObject g in objectsToIgnore){
+            Component[] others = g.gameObject.GetComponentsInChildren(typeof(Collider));
+            foreach(Collider c in others) {
+                otherCols.Add(c);
+            }
+        }
+
+        //Physics.IgnoreCollision(otherCols, thisObjCols);
+        foreach (Collider colT in thisCols)
+        {
+            foreach (Collider colO in otherCols)
+            {
+                Physics.IgnoreCollision(colT, colO);
+                Debug.Log("IGNORED collisions between: " + colT + " and " + colO);
+            }
+        }
+
+    }
+
+
+}
