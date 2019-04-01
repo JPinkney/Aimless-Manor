12d7aad6f67a8600e7233aafb6b221b498373248
vander-heide <33294408+vander-heide@users.noreply.github.com>
2019-04-01 16:56:36 -0400

    new kitchen scene, before adding to hall

---
+++ Assets/Scripts/Infinite Scripts/CameraMovementFull.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovementFull : MonoBehaviour
+{
+    public Transform playerCamera;
+    public Transform front;
+    public Transform back;
+    //public Transform altBack;
+    private void Start()
+    {
+        playerCamera = GameObject.Find("Main Camera").GetComponent<Transform>();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        /*
+        Vector3 playerOffsetFromBack = back.position - playerCamera.position;
+        Vector3 temp = altBack.position + playerOffsetFromBack;
+        temp.y = temp.y * -1;
+        transform.position = temp;
+        */
+
+
+        /*
+        Vector3 playerOffsetFromBack = back.position - playerCamera.position;
+        Vector3 camPosition = front.position + playerOffsetFromBack;
+        camPosition.y = camPosition.y * -1;
+        camPosition.x = camPosition.x * -1;
+        transform.position = camPosition;
+        */
+
+        
+        Vector3 position = transform.position;
+        position[0] = playerCamera.position.x;
+        transform.position = position;
+        //transform.rotation = playerCamera.rotation;
+        //transform.rotation.x = 0.0f;
+
+        Vector3 eulerAngles = playerCamera.rotation.eulerAngles;
+        eulerAngles = new Vector3(eulerAngles.x, eulerAngles.y, eulerAngles.z);
+        transform.rotation = Quaternion.Euler(eulerAngles);
+        //transform.Rotate(Vector3.right, Time.deltaTime * CycleSpeed, Space.World);
+
+
+
+
+        /*
+        //follow player position
+        Vector3 playerOffsetFromPortal = playerCamera.position - otherPortal.position;
+        transform.position = portal.position + playerOffsetFromPortal;
+        */
+
+
+
+
+        //follow player rotation
+        /*
+        float angularDifferenceBetweenPortalRotations = Quaternion.Angle(front.rotation, back.rotation);
+
+        Quaternion portalRotationalDifference = Quaternion.AngleAxis(angularDifferenceBetweenPortalRotations, Vector3.down);
+        Vector3 newCameraDirection = portalRotationalDifference * playerCamera.forward;
+        transform.rotation = Quaternion.LookRotation(newCameraDirection, Vector3.up);
+        */
+        //transform.localRotation = Quaternion.Euler(0, 180, 0);
+
+        //transform.rotation = playerCamera.rotation;
+
+    }
+}
---
+++ Assets/Scripts/Infinite Scripts/RenderMove.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RenderMove : MonoBehaviour
+{
+
+    public Transform playerCamera;
+    public Transform front;
+    public Transform back;
+    public int direc;
+    public float dist;
+    //public Transform altBack;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerCamera = GameObject.Find("Main Camera").GetComponent<Transform>();
+
+
+        /*
+
+        Vector3 positionFromPortal = playerCamera.position - portal.position;
+        //float xPos = positionFromPortal[0];
+        Vector3 position = transform.position;
+        float x = (portal.position.x + positionFromPortal.x) * move;
+        float y = position[1];
+        float z = position[2];
+        transform.position = new Vector3(x, y, z);
+        */
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Vector3 hallSize = front.position - back.position;
+        Vector3 position = transform.position;
+        float x = position[0];
+        float y = position[1];
+        float z = playerCamera.position.z + (dist * direc);
+        transform.position = new Vector3(x, y, z);
+    }
+}
