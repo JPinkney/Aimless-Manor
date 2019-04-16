5e182086e8c21306b8809f5fe595cb8bdd64ab9e
vander-heide <33294408+vander-heide@users.noreply.github.com>
2019-04-16 18:46:01 -0400

    added alternate kitchen puzzle

---
+++ Assets/Scripts/Portal/PortalFollower.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PortalFollower : MonoBehaviour
+{
+    public Transform player;
+    public Transform portal;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.Find("Player").GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 playerPos = player.position;
+        Vector3 camPos = transform.position;
+        Vector3 newPos = new Vector3(playerPos.x, camPos.y, playerPos.z);
+        portal.position = newPos;
+
+
+    }
+}
