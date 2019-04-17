24c7f27b784edd090b278a0c1bbd1a2e90c9fb8a
vander-heide <33294408+vander-heide@users.noreply.github.com>
2019-04-17 00:21:04 -0400

    added kitchen puzzle script

--- Assets/Scripts/Kitchen/KitchenPuzzle.cs
+++ Assets/Scripts/Kitchen/KitchenPuzzle.cs
@@ -6,20 +6,109 @@ public class KitchenPuzzle : MonoBehaviour
 {
     public GameObject doorCollider;
     public GameObject pedestalCollider;
+    public GameObject keyTing;
+
     public GameObject portal;
     public GameObject plate1;
     public GameObject plate2;
     public GameObject plate3;
+    private int cur = 1;
+
+
+    private AudioManager audioManager;
+
+    public AudioClip audio1;
+    private bool play1 = true;
+    private AudioSource source1;
+    public AudioClip audio2;
+    private bool play2 = true;
+    private AudioSource source2;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        keyTing.SetActive(false);
+        portal.SetActive(false);
+        audioManager = AudioManager.instance;
+        source1 = gameObject.AddComponent<AudioSource>();
+        source2 = gameObject.AddComponent<AudioSource>();
+        source1.clip = audio1;
+        source2.clip = audio2;
     }
 
-    // Update is called once per frame
-    void Update()
+
+    public void PlateBroke(int num)
     {
-        
+        if (num == cur)
+        {
+            cur++;
+            PlayAudio(num);
+        }
+        else
+        {
+            cur = 1;
+            //portal.SetActive(true);
+            //doorCollider.SetActive(true);
+            plate1.SetActive(true);
+            plate2.SetActive(true);
+            plate3.SetActive(true);
+            StartCoroutine(Door());
+            //yield return new WaitForSeconds(5);
+            //doorCollider.SetActive(false);
+            //portal.SetActive(false);
+        }
+        if (cur == 4)
+        {
+            //portal.SetActive(true);
+            //pedestalCollider.SetActive(true);
+            StartCoroutine(Pedestal());
+            //yield return new WaitForSeconds(5);
+            //portal.SetActive(false);
+            //pedestalCollider.SetActive(false);
+        }
+
     }
+
+    private void PlayAudio(int num)
+    {
+        if (num == 1)
+        {
+           audioManager.Play(source1);
+        }
+        if (num == 2)
+        {
+            audioManager.Play(source2);
+        }
+    }
+
+
+    IEnumerator Door()
+    {
+        //print(Time.time);
+        //Debug.Log("fuck this shit");
+        portal.SetActive(true);
+        doorCollider.SetActive(true);
+        yield return new WaitForSeconds(0.5f);
+        plate1.SetActive(true);
+        plate2.SetActive(true);
+        plate3.SetActive(true);
+        //Debug.Log("fuck that shit");
+        doorCollider.SetActive(false);
+        portal.SetActive(false);
+        //print(Time.time);
+    }
+    IEnumerator Pedestal()
+    {
+        //print(Time.time);
+        //Debug.Log("fuck this shit");
+        portal.SetActive(true);
+        pedestalCollider.SetActive(true);
+        keyTing.SetActive(true);
+        yield return new WaitForSeconds(0.5f);
+        //Debug.Log("fuck that shit");
+        pedestalCollider.SetActive(false);
+        portal.SetActive(false);
+        //print(Time.time);
+    }
+
 }
